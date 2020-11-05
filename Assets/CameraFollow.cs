7e3ce311f8616842a4eaef838694281ad07be4bf
Galati Gaetan <skyfulle68@gmail.com>
2020-11-05 17:22:51 +0100

    Revert "Maj caméra"
    This reverts commit 5f496f08a175dfff37f68177168807ea2f04f4c5.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,49 +3,20 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
-    [SerializeField]
-    private float smoothing = 5f;
-    [SerializeField]
-    private float zMaxDistance =1;
-    [SerializeField]
-    private GameObject coll;
-
-
-    private float baseZ;
+    public Transform target;
+    public float smoothing = 5f;
     Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
-        baseZ = transform.position.z;
         offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        
         Vector3 targetCamPos = target.position + offset;
-        if (targetCamPos.z < zMaxDistance)
-        {
-            targetCamPos.z = zMaxDistance;
-            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-        }
-
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
-
-    private void OnTriggerEnter(Collider coll)//NE MARCHER PAS JSP PK
-    {
-        Debug.Log("COLID 19");
-        offset.z = baseZ;
-    }
-
-
-
 }
