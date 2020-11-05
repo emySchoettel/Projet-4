5f496f08a175dfff37f68177168807ea2f04f4c5
Galati Gaetan <skyfulle68@gmail.com>
2020-11-05 17:21:11 +0100

    Maj caméra
    La caméra suit le joueur avec un effet « smooth »
    la position en Z de la caméra est bloquée à un, pour éviter qu'elle recule trop
    il faut encore codé un système pour que la position de la caméra se remettent aussi à la position initiale si elle rencontre des collisions

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -3,20 +3,49 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float smoothing = 5f;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float smoothing = 5f;
+    [SerializeField]
+    private float zMaxDistance =1;
+    [SerializeField]
+    private GameObject coll;
+
+
+    private float baseZ;
     Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
+        baseZ = transform.position.z;
         offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        
         Vector3 targetCamPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        if (targetCamPos.z < zMaxDistance)
+        {
+            targetCamPos.z = zMaxDistance;
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        }
+
     }
+
+    private void OnTriggerEnter(Collider coll)//NE MARCHER PAS JSP PK
+    {
+        Debug.Log("COLID 19");
+        offset.z = baseZ;
+    }
+
+
+
 }
