32601c7b3aed2e1b7bb1b8d114291d07ee464f02
emySchoettel <e.schoettel@ludus-academie.com>
2021-01-25 13:25:50 +0100

    Ajout createurs et script pour changer le sprite selon émotions
    Avec un petit todo qui va bien

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    public enum emotionsCreateur
+    {
+        naturel,
+        enerve,
+        triste,
+        espiegle,
+        rire
+
+    }
+    public static void changerCreateurSprite(GameObject createur, emotionsCreateur emotion)
+    {
+        createur.GetComponent<DieuxComportement>().setSprite(emotion);
+    }
+}
