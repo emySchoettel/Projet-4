9a6c6834ddb69fa672dcab543f48fe90f0a13462
Galati Gaetan <skyfulle68@gmail.com>
2021-02-05 14:17:14 +0100

    Revert "Revert "Patch pour le joueur qui s'envole....""
    This reverts commit c000b5e466661c410b3dbe058181f5879a9da828.

--- Assets/Scripts/Mouvements/MouvementGauche.cs
+++ Assets/Scripts/Mouvements/MouvementGauche.cs
@@ -32,7 +32,8 @@ public class MouvementGauche : EtatMouvementJoueur, Observer
         canMove();
         if(EtatMouvementJoueur.canMoveBool)
         {  
-            player.getRigidBody().velocity = new Vector3(x, player.getRigidBody().velocity.y, z) * player.speed;  
+            //player.getRigidBody().velocity = new Vector3(x, player.getRigidBody().velocity.y, z) * player.speed;
+            player.getRigidBody().velocity = new Vector3(x, 0, z) * player.speed;  
         }
     }
     public override void Move(MouvementJoueur player)
