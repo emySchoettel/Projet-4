45e5822c960e8a7c8193f62d044ee3787200bd51
Galati Gaetan <skyfulle68@gmail.com>
2021-02-21 11:55:10 +0100

    TP du hub a la map 1

---
+++ Assets/LoadWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadWorld : MonoBehaviour
+{
+    public string World;
+    private Scene m_Scene;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(World, LoadSceneMode.Single);
+        }
+    }
+
+}
