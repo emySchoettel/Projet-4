7d51c7d01fb6bba7ffa963111f122bdee89d2f94
Galati Gaetan <skyfulle68@gmail.com>
2021-02-24 10:39:58 +0100

    Maj ecran titre

---
+++ Assets/Scripts/TittleScreenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+public class TittleScreenManager: MonoBehaviour { 
+    public string StartScene; 
+    public void StartGame() {  
+        SceneManager.LoadScene(StartScene,LoadSceneMode.Single);  
+    }  
+    public void LoadGame() {  
+        //TODO système de chargement. 
+    }  
+    public void ExitGame() {  
+        Debug.Log("exitgame");  
+        Application.Quit();  
+    }  
+}  
