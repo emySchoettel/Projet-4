be57cb28c5499ee899d46314d1df92b5fbbaa58b
Galati Gaetan <skyfulle68@gmail.com>
2021-03-01 12:40:55 +0100

    Createur

---
+++ Assets/Scripts/Createurs/CreateursSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreateursSceneManager : MonoBehaviour
+{
+    public bool LoadHubFirst = false;
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update()
+    {
+        if (LoadHubFirst){
+            SceneManager.LoadScene("HUB", LoadSceneMode.Additive);
+            LoadHubFirst = false;
+        }
+    }
+}
