b1b00642bd2fe811814fad11241e6526209a997d
emySchoettel <e.schoettel@ludus-academie.com>
2021-03-05 13:23:07 +0100

    premier jet pnj

---
+++ Assets/Scripts/MouvementsPNJ/MouvementPNJ.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementPNJ : MonoBehaviour
+{
+    public List<GameObject> Paths = new List<GameObject>(); 
+    private Rigidbody rigidbody; 
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); 
+        StartCoroutine(mouvement());
+    }
+
+    public IEnumerator mouvement()
+    {
+        foreach(GameObject path in Paths)
+        {
+            for(float i = gameObject.transform.position.z; i < path.transform.position.z; i++)
+            {
+                //rigidbody.velocity = new Vector3()
+            }
+        }
+        
+        yield return null;
+    }
+}
